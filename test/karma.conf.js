d54902bac40d0e587e1ff9888558e902da6a2723
jaime-e <jechegarayz@gmail.com>
2017-01-05 12:28:56 -0300

    [task] include remittance-maker as dependency and modify index to load modules in electron

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/remittance-maker-v2-0-1/index.js',
+      'bower_components/remittance-maker/dist/index.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
