b487e39cdecd14d7040b1a0c8d067c64ab624616
jaime-e <jechegarayz@gmail.com>
2017-01-17 15:33:48 -0300

    [task] load remittance maker from npm

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,7 +29,6 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/remittance-maker/dist/index.min.js',
       'bower_components/a0-angular-storage/dist/angular-storage.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
